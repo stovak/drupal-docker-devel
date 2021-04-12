192ebc8d5a86d1a6547c07ae753946d083c22c21
stovak <119924+stovak@users.noreply.github.com>
2021-04-11 21:47:04 -0700

    makefile WIP

--- Makefile
+++ Makefile
@@ -1,36 +1,5 @@
 
-PROJECT_NAME ?= $(shell basename $(shell git rev-parse --show-toplevel))
-REPO_NAME    ?= ${DOCKER_IMAGE_ORG}/${DOCKER_PROJECT_PREFIX}-${PROJECT_NAME}
-VCS_REF      ?= $(shell git rev-parse --short HEAD)
-DATE_TAG     ?= $(shell TZ=UTC date +%Y-%m-%d_%H.%M)
-VERSION      ?= $(shell git describe --tags --always --dirty --match="v*" 2> /dev/null || cat $(CURDIR)/.version 2> /dev/null || echo v0)
-
-
-## [[ todo: have these be params for the make command ]]
-
-ifndef IMAGE_TAG
-	ifdef CIRCLE_BUILD_NUM
-		IMAGE_TAG:=$(CIRCLE_BUILD_NUM).git$(VCS_REF)
-	else
-		IMAGE_TAG:=latest
-	endif
-endif
-
-ifndef BUILD_ID
-	ifdef CIRCLE_BUILD_NUM
-		BUILD_ID:=${CIRCLE_BUILD_NUM}
-	else
-		BUILD_ID:=${USER}-${VERSION}-$(VCS_REF)
-	endif
-endif
-
-# only need this for circle
-ifdef CIRCLE_BUILD_NUM
-		QUAY := docker login -p "$${DOCKER_IMAGE_HOST}_PASSWD" -u "$${DOCKER_IMAGE_HOST}_USER" ${DOCKER_IMAGE_HOST}
-endif
-
-
-
+include common.mk
 
 
 PHONY: env envrc
@@ -163,11 +132,7 @@ endif
 
 
 
-##
-## help message system
-##
-help: ## Print list of tasks and descriptions.
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-38s\033[0m %s\n", $$1, $$2}'
+
 
 
 how-to-use:  ## Instructions for using this makefile
--- common.mk
+++ common.mk
@@ -1,4 +1,34 @@
 
+PROJECT_NAME ?= $(shell basename $(shell git rev-parse --show-toplevel))
+REPO_NAME    ?= ${DOCKER_IMAGE_ORG}/${DOCKER_PROJECT_PREFIX}-${PROJECT_NAME}
+VCS_REF      ?= $(shell git rev-parse --short HEAD)
+DATE_TAG     ?= $(shell TZ=UTC date +%Y-%m-%d_%H.%M)
+VERSION      ?= $(shell git describe --tags --always --dirty --match="v*" 2> /dev/null || cat $(CURDIR)/.version 2> /dev/null || echo v0)
+
+
+## [[ todo: have these be params for the make command ]]
+
+ifndef IMAGE_TAG
+	ifdef CIRCLE_BUILD_NUM
+		IMAGE_TAG:=$(CIRCLE_BUILD_NUM).git$(VCS_REF)
+	else
+		IMAGE_TAG:=latest
+	endif
+endif
+
+ifndef BUILD_ID
+	ifdef CIRCLE_BUILD_NUM
+		BUILD_ID:=${CIRCLE_BUILD_NUM}
+	else
+		BUILD_ID:=${USER}-${VERSION}-$(VCS_REF)
+	endif
+endif
+
+# only need this for circle
+ifdef CIRCLE_BUILD_NUM
+		QUAY := docker login -p "$${DOCKER_IMAGE_HOST}_PASSWD" -u "$${DOCKER_IMAGE_HOST}_USER" ${DOCKER_IMAGE_HOST}
+endif
+
 
 define getServiceContainer
 ${DOCKER_IMAGE_HOST}/${DOCKER_IMAGE_ORG}/${DOCKER_PROJECT_PREFIX}-${PROJECT_NAME}-$(1)
@@ -20,3 +50,10 @@ endef
 define getGitRepoCloneCommand
 $(shell terminus connection:info $(1).dev --format=json | jq ".git_command")
 endef
+
+
+##
+## help message system
+##
+help: ## Print list of tasks and descriptions.
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-38s\033[0m %s\n", $$1, $$2}'
